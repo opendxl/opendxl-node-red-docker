5e09dfedca49c0019754d43866244fefc25f1d60
Chris Smith <csmith5@mcafee.com>
2018-08-21 08:48:37 -0700

    Update startup.sh
    Added docker host entry into /etc/hosts

--- startup/startup.sh
+++ startup/startup.sh
@@ -3,6 +3,9 @@
 NR_DXL=@opendxl/node-red-contrib-dxl
 NR_PY2_ENV=node-red-py2-env
 
+DOCKER_HOSTNAME=dockerhost
+DOCKER_HOSTIP=$(ip route | sed -n 's/.*default via \([^ ]*\).*/\1/p')
+
 #
 # Function that is invoked when the script fails.
 #
@@ -13,6 +16,19 @@ function fail() {
     echo "Exiting."
     exit 1
 }
+
+# Add entry for docker host to the /etc/hosts file
+if [ -n $DOCKER_HOSTIP ]; then
+    echo "Using docker host IP address ${DOCKER_HOSTIP}"
+    if ! grep -q $DOCKER_HOSTNAME /etc/hosts 2>/dev/null; then
+        echo -e "\n${DOCKER_HOSTIP} ${DOCKER_HOSTNAME}" >> /etc/hosts
+        if [ $? -ne 0 ]; then
+            echo "Failed to add docker host entry to /etc/hosts file" >&2
+        fi
+    fi
+else
+    echo "Unable to determine docker host IP address" >&2
+fi
 
 #
 # Install OpenDXL extensions
